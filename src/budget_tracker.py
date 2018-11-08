32d0bb68b5746ad3bcffb00c488fd94fa220ae5f
callahanhirrel <callahan.hirrel@gmail.com>
2018-11-07 22:07:00 -0600

    More forms work, created prototype database
    Finished login and registration forms, now they just need to be
    connected to the database.

--- src/budget_tracker.py
+++ src/budget_tracker.py
@@ -1,26 +1,47 @@
 # This is the main Flask file for the budget tracker
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '4c1b98566bb86432a39a8d5baa91d184'   # TODO temporary secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'     # set the database file location
+db = SQLAlchemy(app)                                            # Create the instance of SQLAlchemy() we can interact with
 
-# TODO temporary secret key
-app.config['SECRET_KEY'] = '4c1b98566bb86432a39a8d5baa91d184'
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)         # 60 characters due to hashing
+    # TODO relationships:
+    #   var = db.relationship('className', backref='reference in other table', lazy=True)
 
-@app.route("/")
-@app.route("/login")
+    def __repr__(self):
+        return f"User('{self.last_name}', '{self.first_name}', '{self.email}')"
+
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        # TODO if email/password combination matches that of a user:
+        flash(f'Successfully logged in!', 'success')
+        return redirect(url_for('home'))
+        # TODO else, flash('Login unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 @app.route("/about")
 def about():
     return None  # will eventually be render_template()
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/home")
--- src/forms.py
+++ src/forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    first_name = StringField('First name', validators=[DataRequired(), Length(min=1, max=50)])
+    last_name = StringField('Last name', validators=[DataRequired(), Length(min=1, max=50)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
